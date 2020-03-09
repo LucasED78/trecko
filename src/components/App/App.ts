30e3446751f99e2c83a12a935d2a6727c8a48b91
Lucas Eduardo <lucased78@hotmail.com>
2020-03-09 20:39:19 -0300

    [ADDED] Rendering tracking history

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -5,13 +5,21 @@ import Subtitle from "../Subtitle/Subtitle";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 import InputRow from "../Input/InputRow/InputRow";
-import SubtitleWrapper from "../Subtitle/SubtitleWrapper/SubtitleWrapper";
 import HistoryWrapper from "../History/HistoryWrapper/HistoryWrapper";
 import HistoryItem from "../History/HistoryItem/HistoryItem";
 import ContentWrapper from "../ContentWrapper/ContentWrapper";
+import Separator from "../Separator/Separator";
+import TreckoHttpImpl from "../../core/http/impl/TreckoHttpImpl";
+import TrackRepository from "../../core/repository/impl/TrackRepositoryImpl";
+import Track from "../../models/Track";
+import { formatDateTime } from "../../utils/DateUtil";
+import TrackError from "../../models/TrackError";
 
 export default class App implements BaseComponent {
   DOMUtils = new DOMUtils();
+  trackRepository = new TrackRepository(new TreckoHttpImpl());
+  code: string = '';
+  track?: Track | TrackError;
 
   render(): Element {
     const element = this.DOMUtils.createElement(
@@ -24,12 +32,11 @@ export default class App implements BaseComponent {
           basta inserir o código de rastreio logo abaixo e clicar
           no botão.`).render(),
           new InputRow([
-            new Input('Digite o código de rastreio', this.onClickHandler).render(),
-            new Button('Rastrear', console.log).render()
+            new Input('Digite o código de rastreio', this.onChangeHandler).render(),
+            new Button('Rastrear', this.onClickHandler).render()
           ]).render(),
           new HistoryWrapper([
             new Subtitle('Histórico', 'subtitle--start').render(),
-            new HistoryItem('Objeto entrega ao destinatário', '13/12/2019 ás 12:00', 'Vila Olímpia').render()
           ]).render()
         ]).render()
       ]
@@ -38,12 +45,37 @@ export default class App implements BaseComponent {
     return element;
   }
 
-  onClickHandler = (event: Event) => {
+  onChangeHandler = async (event: Event) => {
     const element = event.target as HTMLInputElement;
 
     if (element.style.textTransform !== 'uppercase') {
       element.style.textTransform = 'uppercase';
     } else if (element.value.length == 0) element.style.textTransform = 'initial';
-    
+
+    this.code = element.value;
+  }
+  
+  onClickHandler = async (event: Event) => {
+    this.track = await this.trackRepository.track(this.code);
+
+    if (this.track){
+      if (this.track instanceof Track){
+        const items = this.track.tracks.reverse().length;
+        const element = this.track.tracks.map((e, i) => {
+          if (i < items - 1){
+            return this.DOMUtils.createElement('div', '', [
+              new HistoryItem(e.status, formatDateTime(e.trackedAt), e.local).render(),
+              new Separator().render()
+            ])
+          }
+          else return new HistoryItem(e.status, formatDateTime(e.trackedAt), e.local).render();
+        })
+
+        const container = document.querySelector('.history__wrapper');
+        
+        if (container)
+          this.DOMUtils.appendChild(container, element);
+      } else console.log(this.track);
+    }
   }
 }
